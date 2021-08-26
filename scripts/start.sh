7fd19e3cb678399fc9048f338b49bc0b225f13c0
KangJuO <juy5437@gmail.com>
2021-08-26 20:42:29 +0900

    fix : json file, add(start.sh): ssm root

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/BRRRRng/brrrrng-server
+
+export MONGO_URI=$(aws ssm get-parameters --region ap-northeast-2 --names MONGO_URI --query Parameters[0].Value | sed 's/"//g')
+export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_ENV --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start index.js

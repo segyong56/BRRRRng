10c54a561697de39bef3e545bab1a339c5c4112a
segyong <segyong56@gmail.com>
2021-08-30 04:37:19 +0900

    edit:index파일

--- brrrrng-client/src/index.js
+++ brrrrng-client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import store from './store'
-import reportWebVitals from './reportWebVitals';
+// import store from './store'
+// import reportWebVitals from './reportWebVitals';
 
 import Reducer from './_reducers/index';
 

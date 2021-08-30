c899fb5b54d9c7aa9ec0e13bdcdfc24d6c301252
KangJuO <juy5437@gmail.com>
2021-08-30 23:02:48 +0900

    edit: cors

--- brrrrng-server/index.js
+++ brrrrng-server/index.js
@@ -12,7 +12,12 @@ const port = 80;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  }),
+);
 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);

e4aad81ad855f56781a7348b63d0b3a52daed1b1
KangJuO <juy5437@gmail.com>
2021-08-31 10:31:08 +0900

    mod: cors 변경

--- brrrrng-server/index.js
+++ brrrrng-server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000, https://brrrrng.ga",
     allowedHeaders: ["Content-Type"],
     credentials: true,
   }),

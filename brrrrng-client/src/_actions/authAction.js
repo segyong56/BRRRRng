43dc80415290f98732a52d20333050aec50bf08b
KangJuO <juy5437@gmail.com>
2021-08-31 23:39:52 +0900

    fix: fix build error

--- brrrrng-client/src/_actions/authAction.js
+++ brrrrng-client/src/_actions/authAction.js
@@ -1,71 +1,66 @@
-import {
-  SUCCESS_SIGNUP,
-  SUCCESS_LOGIN,
-  SUCCESS_LOGOUT,
-  AUTH_USER,
-} from "./types.js";
+// import {
+//   SUCCESS_SIGNUP,
+//   SUCCESS_LOGIN,
+//   SUCCESS_LOGOUT,
+//   AUTH_USER,
+// } from "./types.js";
 
-import axios from "axios";
-const API_URL = "https://api.brrrrng.ga";
+// import axios from "axios";
+// const API_URL = "https://api.brrrrng.ga";
 
-export const signupRequest = (data) => {
-  const request = axios
-    .post(`${API_URL}/auth/signup`, data)
-    .then((response) => response.data);
+// export const signupRequest = (data) => {
+//   const request = axios
+//     .post(`${API_URL}/auth/signup`, data)
+//     .then((response) => response.data);
 
-  return {
-    type: SUCCESS_SIGNUP,
-    payload: request,
-  };
-};
+//   return {
+//     type: SUCCESS_SIGNUP,
+//     payload: request,
+//   };
+// };
 
-export const loginRequest = (data) => {
-  const request = axios
-    .post(`${API_URL}/auth/login`, data, { withCredentials: true })
-    .then((response) => {
-      return response.data;
-    });
-  console.log(request);
-  return {
-    type: SUCCESS_LOGIN,
-    payload: request,
-  };
-};
+// export const loginRequest = (data) => {
+//   const request = axios
+//     .post(`${API_URL}/auth/login`, data, { withCredentials: true })
+//     .then((response) => {
+//       return response.data;
+//     });
+//   console.log(request);
+//   return {
+//     type: SUCCESS_LOGIN,
+//     payload: request,
+//   };
+// };
 
-<<<<<<< HEAD
-export const logoutRequest = () => {
-  const request = axios
-    .get(`${API_URL}/auth/logout`, { withCredentials: true })
-    .then((response) => {
-      console.log(response);
-      return response.data;
-    });
-=======
-export const logoutRequest = (accessToken) => {
+// export const logoutRequest = (accessToken) => {
+//   const request = axios
+//     .get(
+//       `${API_URL}/auth/logout`,
+//       { withCredentials: true },
+//       {
+//         headers: {
+//           authorization: accessToken,
+//         },
+//       },
+//     )
+//     .then((response) => {
+//       console.log(response);
+//       return response.data;
+//     });
 
-  const request = axios.get(`${API_URL}/auth/logout`,{withCredentials: true}, {
-    headers: {
-      authorization: accessToken
-    }
-  }).then(response => {
-    console.log(response)
-    return response.data
-  })
->>>>>>> d295a628a7a6a2d2bc4ffd88f6b36c9b8ccfd0d2
+//   return {
+//     type: SUCCESS_LOGOUT,
+//     payload: request,
+//   };
+// };
 
-  return {
-    type: SUCCESS_LOGOUT,
-    payload: request,
-  };
-};
+// export const auth = () => {
+//   const request = axios
+//     .get(`${API_URL}/auth/authtest`)
+//     .then((response) => response.data);
 
-export const auth = () => {
-  const request = axios
-    .get(`${API_URL}/auth/authtest`)
-    .then((response) => response.data);
-
-  return {
-    type: AUTH_USER,
-    payload: request,
-  };
-};
+//   return {
+//     type: AUTH_USER,
+//     payload: request,
+//   };
+// };
